29d47f90f4b762be49e6d8f74ea31f2c35c29602
Kamil Laptop <kkw.kaminski@gmail.com>
2023-12-26 20:46:18 +0100

    Poprawki wg uwag z github oraz wg pep8

--- wizytownik_v2.py
+++ wizytownik_v2.py
@@ -1,21 +1,22 @@
 from faker import Faker
 
+
 class BaseContact:
     def __init__(self, imie, nazwisko, telefon, email):
         self.imie = imie
         self.nazwisko = nazwisko
         self.telefon = telefon
         self.email = email
-        self._label_length = None
+        self._label_length = len(f"{self.imie} {self.nazwisko}")
 
     def contact(self):
-        print(f"Wybieram numer {self.telefon} i dzwonię do {self.imie} {self.nazwisko}")
+        print(
+            f"Wybieram numer {self.telefon} i dzwonię do {self.imie} {self.nazwisko}")
 
     @property
     def label_length(self):
-        if self._label_length is None:
-            self._label_length = len(f"{self.imie} {self.nazwisko}")
-        return self._label_length
+        self._label_length = len(f"{self.imie} {self.nazwisko}")
+
 
 class BusinessContact(BaseContact):
     def __init__(self, stanowisko, firma, telefon_sluzbowy, *args, **kwargs):
@@ -25,13 +26,9 @@ class BusinessContact(BaseContact):
         self.telefon_sluzbowy = telefon_sluzbowy
 
     def contact(self):
-        print(f"Wybieram numer {self.telefon_sluzbowy} i dzwonię do {self.imie} {self.nazwisko} w firmie {self.firma}")
+        print(
+            f"Wybieram numer {self.telefon_sluzbowy} i dzwonię do {self.imie} {self.nazwisko} w firmie {self.firma}")
 
-    @property
-    def label_length(self):
-        if self._label_length is None:
-            self._label_length = len(f"{self.imie} {self.nazwisko}")
-        return self._label_length
 
 def create_contacts(rodzaj, ilosc=5):
     fake = Faker()
@@ -49,11 +46,13 @@ def create_contacts(rodzaj, ilosc=5):
             stanowisko = fake.job()
             firma = fake.company()
             telefon_sluzbowy = fake.phone_number()
-            contact = BusinessContact(stanowisko=stanowisko, firma=firma, telefon_sluzbowy=telefon_sluzbowy, imie=imie, nazwisko=nazwisko, telefon=telefon, email=email)
+            contact = BusinessContact(stanowisko=stanowisko, firma=firma, telefon_sluzbowy=telefon_sluzbowy,
+                                      imie=imie, nazwisko=nazwisko, telefon=telefon, email=email)
 
         contacts.append(contact)
 
     return contacts
+
 
 if __name__ == "__main__":
     base_contacts = create_contacts('base')
